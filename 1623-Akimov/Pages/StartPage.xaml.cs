b7b54d1ee734a40846d8ef26eba7078772a992eb
Zoom656 <Zoomgloom@gmail.com>
2024-05-27 21:38:18 +0300

    Добавил стиль кнопок, убрал все связанные с ним свойства со всех страниц

---
+++ 1623-Akimov/Pages/StartPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _1623_Akimov.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для StartPage.xaml
+    /// </summary>
+    public partial class StartPage : Window
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+
+        private void menuExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+ 
+}
